db5d444ca04f69ca7b87313b1be6150089ae20d3
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-09-02 22:30:40 +0530

    Add even numbers counter

---
+++ Day5/count_even_numbers.py
@@ -0,0 +1,15 @@
+count = 0
+
+for i in range(1, 21):
+    if i % 2 == 0:
+        count = count + 1
+
+print("Total even numbers =", count)
+
+Output:
+
+Total even numbers = 10
+
+Commit message:
+
+Add even numbers counter
