77a672f76c2badf04f379a10fbb5905b8476ef55
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-09-09 22:06:24 +0530

    Add dictionary key check for student age

---
+++ Day12/check_dictionary_key.py
@@ -0,0 +1,10 @@
+student = {
+    "name": "Vaibhavi",
+    "age": 20,
+    "course": "CSE"
+}
+
+if "age" in student:
+    print("Age is available in the dictionary")
+else:
+    print("Age is not available")
