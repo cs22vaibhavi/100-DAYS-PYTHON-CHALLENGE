7a068e05b896b3981998e5fe6f22560ddc6aef47
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-09-21 22:09:57 +0530

    Add function to filter even numbers from a list

---
+++ Day24/Filter_even_function.py
@@ -0,0 +1,13 @@
+def get_even_numbers(numbers):
+    even_numbers = []
+
+    for number in numbers:
+        if number % 2 == 0:
+            even_numbers.append(number)
+
+    return even_numbers
+
+
+numbers = [1, 2, 3, 4, 5, 6]
+
+print("Even numbers =", get_even_numbers(numbers))
